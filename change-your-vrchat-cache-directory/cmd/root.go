2fc7eed6042b0276efa0c740fe151afee106b7d8
CunYu <cunyu@liuli.lol>
2020-09-06 18:08:55 +0800

    更友好的提示

--- change-your-vrchat-cache-directory/cmd/root.go
+++ change-your-vrchat-cache-directory/cmd/root.go
@@ -19,30 +19,18 @@ var (
 			defer func() {
 				if err := recover(); err != nil {
 					fmt.Println(err)
+					os.Exit(2)
 				}
 			}()
 			var yn string
 			var err error
-			if configFile != "" {
-				fmt.Printf("寻找到VRChat配置文件 %s\n确认使用该配置文件吗? (Y/n)\n", configFile)
-				fmt.Scanln(&yn)
+			if configFile == "" && cfd != "" {
+				configFile = cfd
+				fmt.Printf("寻找到VRChat配置文件 %s\n确认使用该配置文件吗? (Y/n) ", configFile)
+				fmt.Scan(&yn)
 				if strings.ToUpper(yn) != "Y" {
 					panic(`请使用运行参数"--config"指定配置文件.`)
 				}
-			}
-			if cacheDirectory == "" {
-				fmt.Println("请输入新的缓存目录:")
-				fmt.Scan(&cacheDirectory)
-			}
-			cacheDirectory, err = filepath.Abs(cacheDirectory)
-			if err != nil {
-				panic(err)
-			}
-			info, err := os.Stat(cacheDirectory)
-			if err != nil {
-				panic(err)
-			} else if !info.IsDir() {
-				panic(`指定的目标不是一个有效的文件夹, 请使用运行参数"--cache"指定缓存目录.`)
 			}
 			data, err := ioutil.ReadFile(configFile)
 			if err != nil {
@@ -52,12 +40,34 @@ var (
 			if err := json.Unmarshal(data, &config); err != nil {
 				panic(err)
 			}
-			config["cache_directory"] = cacheDirectory
+			newCacheDir := cacheDirectory
+			if cacheDirectory == "" {
+				if nowCacheDir, ok := config["cache_directory"]; ok {
+					fmt.Println("当前缓存目录:", nowCacheDir)
+				}
+				fmt.Println("请输入新的缓存目录:")
+				fmt.Scan(&newCacheDir)
+			}
+			newCacheDir, err = filepath.Abs(newCacheDir)
+			if err != nil {
+				panic(err)
+			}
+			info, err := os.Stat(newCacheDir)
+			if err != nil {
+				panic(err)
+			} else if !info.IsDir() {
+				panic(`指定的目标不是一个有效的文件夹, 请使用运行参数"--cache"指定缓存目录.`)
+			}
+			config["cache_directory"] = newCacheDir
 			if data, err = json.Marshal(&config); err != nil {
 				panic(err)
 			}
 			if err := ioutil.WriteFile(configFile, data, 0655); err != nil {
 				panic(err)
+			}
+			if cacheDirectory == "" {
+				fmt.Println("修改成功, VRChat下一次启动后生效. (Windows可直接关闭本程序窗口)")
+				fmt.Scan(&yn)
 			}
 		},
 	}
@@ -66,12 +76,14 @@ var (
 	configFile     string
 )
 
+var cfd string
+
 func init() {
-	cfd := filepath.Join(appdata, "LocalLow\\VRChat\\VRChat\\config.json")
+	cfd = filepath.Join(appdata, "LocalLow\\VRChat\\VRChat\\config.json")
 	if _, err := os.Stat(cfd); err != nil {
 		cfd = ""
 	}
-	rootCmd.PersistentFlags().StringVarP(&configFile, "config", "c", cfd, "VRChat config.json")
+	rootCmd.PersistentFlags().StringVarP(&configFile, "config", "c", "", "VRChat config.json")
 	rootCmd.PersistentFlags().StringVarP(&cacheDirectory, "cache", "d", "", "Cache Directory")
 }
 
